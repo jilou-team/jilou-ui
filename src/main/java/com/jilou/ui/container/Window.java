433ea962149d9f281d432339bd7643444637500b
exepta <genshin.exepta@outlook.com>
2024-11-30 11:51:42 +0100

    feat(#7): create windows and user test them

---
+++ src/main/java/com/jilou/ui/container/Window.java
@@ -0,0 +1,41 @@
+package com.jilou.ui.container;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.lwjgl.opengl.GL11;
+
+public class Window extends AbstractWindow {
+
+    private static final Logger LOGGER = LogManager.getLogger(Window.class);
+
+    public Window(String localizedName) {
+        super(localizedName);
+    }
+
+    @Override
+    protected void setup() {
+        /**/
+    }
+
+    @Override
+    protected void update(float delta) {
+        /**/
+    }
+
+    @Override
+    protected void render() {
+        GL11.glBegin(GL11.GL_TRIANGLES);
+        GL11.glColor3f(1.0f, 0.0f, 0.0f);
+        GL11.glVertex2f(-0.5f, -0.5f);
+        GL11.glColor3f(0.0f, 1.0f, 0.0f);
+        GL11.glVertex2f(0.5f, -0.5f);
+        GL11.glColor3f(0.0f, 0.0f, 1.0f);
+        GL11.glVertex2f(0.0f, 0.5f);
+        GL11.glEnd();
+    }
+
+    @Override
+    protected void destroy() {
+        /**/
+    }
+}
--- src/main/java/examples/ExampleWindowTest.java
+++ src/main/java/examples/ExampleWindowTest.java
@@ -1,11 +1,19 @@
 package examples;
 
 import com.jilou.ui.JilouUI;
+import com.jilou.ui.container.Window;
+import com.jilou.ui.enums.Backend;
 
 public class ExampleWindowTest {
 
     public static void main(String[] args) {
         JilouUI.load(args);
+
+        Window window = new Window("Test");
+        window.setBackend(Backend.VULKAN);
+        window.start();
+        window.show();
+        window.setTitle("Example Window");
     }
 
 }
